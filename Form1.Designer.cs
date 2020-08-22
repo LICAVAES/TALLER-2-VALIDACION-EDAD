d08a326b1fc6b3c68ef6aaae026dbad61f828bd3
CATALINA VANEGAS <licavaes@gmail.com>
2020-08-22 18:37:59 -0500

    Se realiza ejercicio de validacion edad C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Taller_2_ValidacionEdad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnValidar = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtEdad = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VALIDACION DE EDAD";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(415, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 142);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnValidar
+            // 
+            this.BtnValidar.Location = new System.Drawing.Point(27, 171);
+            this.BtnValidar.Name = "BtnValidar";
+            this.BtnValidar.Size = new System.Drawing.Size(159, 54);
+            this.BtnValidar.TabIndex = 1;
+            this.BtnValidar.Text = "VALIDAR";
+            this.BtnValidar.UseVisualStyleBackColor = true;
+            this.BtnValidar.Click += new System.EventHandler(this.BtnValidar_Click);
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(217, 171);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(153, 54);
+            this.BtnLimpiar.TabIndex = 2;
+            this.BtnLimpiar.Text = "LIMPIAR";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Location = new System.Drawing.Point(464, 257);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(138, 47);
+            this.BtnSalir.TabIndex = 3;
+            this.BtnSalir.Text = "SALIR";
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ingrese su  Edad";
+            // 
+            // TxtEdad
+            // 
+            this.TxtEdad.Location = new System.Drawing.Point(217, 92);
+            this.TxtEdad.Name = "TxtEdad";
+            this.TxtEdad.Size = new System.Drawing.Size(99, 20);
+            this.TxtEdad.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 312);
+            this.ControlBox = false;
+            this.Controls.Add(this.TxtEdad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnSalir);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.BtnValidar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "VALIDACION EDAD";
+           
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BtnValidar;
+        private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtEdad;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller_2_ValidacionEdad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnValidar_Click(object sender, EventArgs e)
+        {
+            int edad = int.Parse(TxtEdad.Text);
+            if (TxtEdad.Text.Length >2 )
+            {
+                MessageBox.Show("LA EDAD INGRESADA NO ES PERMITIDA","ERROR", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                TxtEdad.Text = String.Empty;
+                TxtEdad.Focus();
+            } else if (edad > 18)
+         
+            {
+                MessageBox.Show("ES MAYOR DE EDAD", "MENSAJE DEL SISTEMA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("ES MENOR DE EDAD", "EROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            TxtEdad.Text = String.Empty;
+            TxtEdad.Focus();
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+             
+    }
+}
